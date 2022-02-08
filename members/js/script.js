431b48211c36e1e005d85735d7e2e2e85f2465bb
Ahmed Saber <ahmed0saber33@gmail.com>
2022-02-08 18:43:52 +0200

    Add files via upload

---
+++ members/js/script.js
@@ -0,0 +1,19 @@
+// JavaScript Document
+ $(document).ready(function() {
+    $('#autoWidth').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth').removeClass('cS-hidden');
+        } 
+    });  
+  });
+  $(document).ready(function() {
+    $('#autoWidth2').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth2').removeClass('cS-hidden');
+        } 
+    });  
+  });
